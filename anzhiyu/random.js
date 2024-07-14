dea6e9a359f512c0e5cc46123d0cb1f72709538e
Ah Nan <xan13790@gmail.com>
2024-07-14 17:45:58 +0800

    Site updated: 2024-07-14 17:45:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/14/hello-world/","2024/07/14/我的第一篇博客/"];function toRandomPost(){
+var posts=["2024/07/14/hello-world/","2024/07/14/odoo完成开发练习/","2024/07/14/我的第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
