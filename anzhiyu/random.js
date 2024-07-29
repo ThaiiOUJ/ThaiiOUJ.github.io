4f6aaf19680bdc924cb400200d4880583d1e3195
nijingnan <nijingnan@qq.com>
2024-07-30 02:25:56 +0800

    Site updated: 2024-07-30 02:25:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/19/Case/","2024/07/28/Css/","2024/06/15/Git/","2024/07/27/GitLab/","2024/06/25/Mysql—救急！/","2024/07/28/Html/","2024/07/15/odoo继承/","2024/07/11/JavaScript/","2024/07/14/odoo完成开发练习/"];function toRandomPost(){
+var posts=["2024/07/19/Case/","2024/07/28/Css/","2024/07/28/Html/","2024/06/15/Git/","2024/07/27/GitLab/","2024/06/25/Mysql—救急！/","2024/07/14/odoo完成开发练习/","2024/07/15/odoo继承/","2024/07/11/JavaScript/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
